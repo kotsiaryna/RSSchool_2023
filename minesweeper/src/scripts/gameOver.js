980256c98f8f76f9f94ae9c615d8096f0617e1d4
Katsiaryna <katsiaryna.n@gmail.com>
2023-05-17 22:44:44 +0300

    fix: win message

--- minesweeper/src/scripts/gameOver.js
+++ minesweeper/src/scripts/gameOver.js
@@ -4,10 +4,11 @@ import {timerID, newID} from "../index"
 import Tumblers from "../tumblers"
 import savetoLocal from "./saveResults"
 import { appendResults } from "./showResults"
+import { clickNumber, timeValue } from "../counters"
 
 function gameOver(isWin) {
  Tumblers.after(GameOverMessage)
-  GameOverMessage.textContent = isWin ? 'Hoorray!!! Your are winner!!!' : 'Ooops! You are exposed. Game over.'
+  GameOverMessage.textContent = isWin ? `Hooray! You found all mines in ${timeValue.textContent} and ${clickNumber.textContent} moves!` : 'Game over. Try again.'
   GameOverMessage.style.color = isWin ? 'green' : 'red'
 
   savetoLocal(isWin)
