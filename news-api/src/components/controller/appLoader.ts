d1add6243edeb5e0819a667161651b50ac130c03
Katsiaryna <katsiaryna.n@gmail.com>
2023-06-11 18:26:12 +0300

    fix: change api link

--- news-api/src/components/controller/appLoader.ts
+++ news-api/src/components/controller/appLoader.ts
@@ -2,7 +2,7 @@ import Loader from './loader';
 
 class AppLoader extends Loader {
     constructor() {
-        super('https://newsapi.org/v2/', {
+        super('https://rss-news-api.onrender.com/', {
             apiKey: 'a6748dc91b9e4f7a8af5cc41a1090947', // получите свой ключ https://newsapi.org/
         });
     }
