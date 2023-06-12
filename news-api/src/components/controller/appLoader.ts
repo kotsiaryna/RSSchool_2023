2d5570180cd809bc2c1f0b111249e5b3d15a117f
Katsiaryna <katsiaryna.n@gmail.com>
2023-06-12 23:38:37 +0300

    fix: remove API key

--- news-api/src/components/controller/appLoader.ts
+++ news-api/src/components/controller/appLoader.ts
@@ -3,7 +3,7 @@ import Loader from './loader';
 class AppLoader extends Loader {
     constructor() {
         super('https://rss-news-api.onrender.com/', {
-            apiKey: 'a6748dc91b9e4f7a8af5cc41a1090947', // получите свой ключ https://newsapi.org/
+            apiKey: '', // получите свой ключ https://newsapi.org/
         });
     }
 }
