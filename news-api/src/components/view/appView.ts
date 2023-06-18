3608d8bdeb7d874f186bdedcbb6b1d6d29e93969
Katsiaryna <katsiaryna.n@gmail.com>
2023-06-18 11:25:31 +0300

    fix: correct expression after ? in appView.ts

--- news-api/src/components/view/appView.ts
+++ news-api/src/components/view/appView.ts
@@ -12,12 +12,12 @@ export class AppView {
     }
 
     public drawNews(data?: Partial<ResponseObj>): void {
-        const values = data?.articles ? data?.articles : [];
+        const values = data?.articles ? data.articles : [];
         this.news.draw(values);
     }
 
     public drawSources(data?: Partial<ResponseObj>): void {
-        const values = data?.sources ? data?.sources : [];
+        const values = data?.sources ? data.sources : [];
         this.sources.draw(values);
     }
 }
