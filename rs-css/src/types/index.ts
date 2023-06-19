50c5015c3e6f28a97b14bc170f7b4f5624e54d36
Katsiaryna <katsiaryna.n@gmail.com>
2023-06-19 16:48:23 +0300

    feat: add elements creation util

---
+++ rs-css/src/types/index.ts
@@ -0,0 +1,6 @@
+export interface Elem {
+    tag: string;
+    classNames: string[];
+    textContent: string;
+    callback: (e: Event) => void;
+}
---
+++ rs-css/src/utils/createElement.ts
@@ -0,0 +1,46 @@
+import { Elem } from '../types/index';
+export default class ElementCreator {
+    private element;
+
+    constructor(options: Elem) {
+        this.element = document.createElement(options.tag);
+        this.makeElement(options);
+    }
+
+    public getElement(): HTMLElement {
+        return this.element;
+    }
+
+    public addInnerElement(element: HTMLElement): void {
+        if (element && element instanceof ElementCreator) {
+            this.element?.append(element.getElement());
+        } else {
+            this.element.append(element);
+        }
+    }
+
+    private makeElement(options: Elem): void {
+        // this.element = document.createElement(options.tag);
+        this.setCssClasses(options.classNames);
+        this.setTextContent(options.textContent);
+        if (options.callback) {
+            this.setCallback(options.callback);
+        }
+    }
+
+    private setCssClasses(cssClasses: Elem['classNames'] = []): void {
+        this.element?.classList.add(...cssClasses);
+    }
+
+    private setTextContent(Text = ''): void {
+        if (this.element) {
+            this.element.textContent = Text;
+        }
+    }
+
+    private setCallback(callback: Elem['callback']): void {
+        if (callback) {
+            this.element.addEventListener('click', (event) => callback(event));
+        }
+    }
+}
