5d900bc3574133c7995fbdf0befe7c46a889c4c8
Katsiaryna <katsiaryna.n@gmail.com>
2023-06-23 11:08:59 +0300

    feat: add first 3 levels

--- rs-css/src/components/view/levels/levels.ts
+++ rs-css/src/components/view/levels/levels.ts
@@ -1,27 +1,28 @@
 import { Level } from '../../../types/index';
 
-export const level1: Level = {
+const level1: Level = {
     id: 1,
     name: 'Level 1',
-    code: '<div>&lt;div class="table"&gt;<div>&lt;plate /&gt;</div><div>&lt;plate /&gt;</div>&lt;/div&gt;</div>',
-    task: 'Select all oranges',
+    code: '<div>&lt;div class="sofa"&gt;<div>&lt;cat/&gt;</div><div>&lt;cat /&gt;</div>&lt;/div&gt;</div>',
+    task: 'Select all cats',
     hint: '',
-    tableCode: '<div class="plate"></div><div class="plate"></div>',
+    tableCode: '<sofa><cat class="active"></cat><cat class="active"></cat></sofa>',
 };
 const level2: Level = {
     id: 2,
     name: 'Level 2',
-    code: '',
-    task: 'Select all oranges',
-    hint: '',
-    tableCode: '',
+    code: '<div>&lt;div class="sofa"&gt;<div>&lt;rabbit/&gt;</div><div>&lt;cat/&gt;</div><div>&lt;croco/&gt;</div><div>&lt;cat /&gt;</div>&lt;/div&gt;</div>',
+    task: 'Select all who are not a cat',
+    hint: 'Select all elements that do not have a specific tag using the :not() selector',
+    tableCode: '<sofa><rabbit class="active"></rabbit><cat></cat><croco class="active"></croco><cat></cat></sofa>',
 };
 const level3: Level = {
     id: 3,
     name: 'Level 3',
-    code: '',
-    task: 'Select all oranges',
+    code: '<div>&lt;div class="sofa"&gt;<div>&lt;chicken/&gt;</div><div>&lt;chicken/&gt;</div><div>&lt;chicken/&gt;</div><div>&lt;chicken/&gt;</div><div>&lt;chicken/&gt;</div>&lt;/div&gt;</div>',
+    task: 'Select all even animals',
     hint: '',
-    tableCode: '',
+    tableCode:
+        '<sofa><chicken class="active"></chicken><chicken></chicken><chicken class="active"></chicken><chicken></chicken><chicken class="active"></chicken></sofa>',
 };
 export const levels: Level[] = [level1, level2, level3];
