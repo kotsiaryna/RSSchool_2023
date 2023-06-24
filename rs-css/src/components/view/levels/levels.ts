f5ddfe87a9ee8374f6af15c6811725b5d61b7503
Katsiaryna <katsiaryna.n@gmail.com>
2023-06-24 14:30:23 +0300

    fix: correct level1

--- rs-css/src/components/view/levels/levels.ts
+++ rs-css/src/components/view/levels/levels.ts
@@ -3,18 +3,20 @@ import { Level } from '../../../types/index';
 const level1: Level = {
     id: 1,
     name: 'Level 1',
-    code: '<div>&lt;div class="sofa"&gt;<div>&lt;cat/&gt;</div><div>&lt;cat /&gt;</div>&lt;/div&gt;</div>',
+    code: '<div>&lt;div class="sofa"&gt;<div>&lt;cat/&gt;</div><div>&lt;cat/&gt;</div><div>&lt;cat /&gt;</div>&lt;/div&gt;</div>',
     task: 'Select all cats',
     hint: '',
-    tableCode: '<sofa><cat class="active"></cat><cat class="active"></cat></sofa>',
+    tableCode:
+        '<sofa class="level1"><cat class="active"></cat><cat class="active"></cat><cat class="active"></cat></sofa>',
 };
 const level2: Level = {
     id: 2,
     name: 'Level 2',
-    code: '<div>&lt;div class="sofa"&gt;<div>&lt;rabbit/&gt;</div><div>&lt;cat/&gt;</div><div>&lt;croco/&gt;</div><div>&lt;cat /&gt;</div>&lt;/div&gt;</div>',
+    code: '<div>&lt;div class="sofa"&gt;<div>&lt;rabbit/&gt;</div><div>&lt;dog/&gt;</div><div>&lt;cat/&gt;</div><div>&lt;croco/&gt;</div><div>&lt;cat /&gt;</div>&lt;/div&gt;</div>',
     task: 'Select all who are not a cat',
     hint: 'Select all elements that do not have a specific tag using the :not() selector',
-    tableCode: '<sofa><rabbit class="active"></rabbit><cat></cat><croco class="active"></croco><cat></cat></sofa>',
+    tableCode:
+        '<sofa><rabbit class="active"></rabbit><dog class="active"></dog><cat></cat><croco class="active"></croco><cat></cat></sofa>',
 };
 const level3: Level = {
     id: 3,
@@ -25,4 +27,14 @@ const level3: Level = {
     tableCode:
         '<sofa><chicken class="active"></chicken><chicken></chicken><chicken class="active"></chicken><chicken></chicken><chicken class="active"></chicken></sofa>',
 };
-export const levels: Level[] = [level1, level2, level3];
+const level4: Level = {
+    id: 4,
+    name: 'Level 4',
+    code: '<div>&lt;div class="sofa"&gt;<div>&lt;dachshund/&gt;</div><div>&lt;dachshund/&gt;</div><div>&lt;dachshund/&gt;</div><div>&lt;dachshund/&gt;</div>&lt;/div&gt;</div>',
+    task: 'Select all animals, that are immediate on the top of a dachshund',
+    hint: '',
+    tableCode:
+        '<sofa><dachshund><pig class="active"></pig></dachshund><dachshund><rabbit class="active"><pig></pig></rabbit></dachshund><dachshund></dachshund><dachshund><chicken class="active"></chicken></dachshund></sofa>',
+};
+
+export const levels: Level[] = [level1, level2, level3, level4];
