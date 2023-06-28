6abb7e865e6d60d0e3bdb582b58b1da2d539d51a
Katsiaryna <katsiaryna.n@gmail.com>
2023-06-28 18:33:38 +0300

    feat: add level1 with hljs

--- rs-css/src/components/view/game/editor/html-markup-view.ts
+++ rs-css/src/components/view/game/editor/html-markup-view.ts
@@ -1,8 +1,10 @@
+import hljs from 'highlight.js';
 import { Elem, Level } from '../../../../types/index';
 import { levels } from '../../levels/levels';
 import View from '../../view';
 
 export default class HTMLMarkupView extends View {
+    private appendPlace = document.createElement('span');
     constructor() {
         const options: Elem = {
             tag: 'div',
@@ -12,6 +14,38 @@ export default class HTMLMarkupView extends View {
         this.addStartLevel(levels[0]);
     }
     private addStartLevel(level: Level): void {
-        this.makeView.getElement().innerHTML = level.code;
+        const sofa = document.createElement('div');
+        sofa.className = 'sofa';
+
+        this.appendPlace.textContent = '<div class = "sofa">';
+        hljs.highlightElement(this.appendPlace);
+
+        const lastSpan = document.createElement('span');
+        lastSpan.textContent = '</div>';
+        hljs.highlightElement(lastSpan);
+
+        sofa.append(this.appendPlace, lastSpan);
+
+        if (lastSpan.firstElementChild) {
+            lastSpan.firstElementChild.className = 'hljs-keyword';
+        }
+
+        this.addAnimals(level);
+
+        this.makeView.getElement().append(sofa);
+        console.log(level.id);
+    }
+
+    private addAnimals(level: Level): void {
+        if (level.code) {
+            for (let i = 0; i < level.code?.length; i++) {
+                const element = document.createElement('div');
+                level.code?.map((el) => {
+                    element.textContent = `<${el}/>`;
+                });
+                hljs.highlightElement(element);
+                this.appendPlace.after(element);
+            }
+        }
     }
 }
