a9a5164c7c5ebe615ab91227c01cb1bbf108166e
Katsiaryna <katsiaryna.n@gmail.com>
2023-07-16 13:19:48 +0300

    feat: add page updating

---
+++ async-race/src/render/updatePage.ts
@@ -0,0 +1,22 @@
+import getPage from "../api/getPage";
+// eslint-disable-next-line import/no-cycle
+import createTrack from "../components/garage/main/track";
+
+export default async function updatePage(
+  n: number,
+  place: Element,
+): Promise<void> {
+  const actualCars = await getPage(n);
+  console.log(actualCars);
+
+  const currentCars = [...place.children].filter((elem) =>
+    elem.classList.contains("track"),
+  );
+  console.log(currentCars);
+  currentCars.forEach((car) => car.remove());
+
+  actualCars.forEach((car) => {
+    const track = createTrack(car);
+    place.append(track);
+  });
+}
