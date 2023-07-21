93696aaee87ecfa238249d9ac4a8619950b5130f
Katsiaryna <katsiaryna.n@gmail.com>
2023-07-21 15:26:22 +0300

    fix: saving winners state after new race

--- async-race/src/render/updateWinnerTable.ts
+++ async-race/src/render/updateWinnerTable.ts
@@ -1,6 +1,9 @@
-import { getWinners } from "../api/getData";
+import { editWinners, getWinners } from "../api/getData";
+import getSortedWinners from "../api/getSortedWinners";
 import winner from "../components/winner/winners";
 import { addWinners } from "../components/winner/winners-table";
+import fillTable from "./fillTable";
+// import { Winner } from "../types/type";
 
 export default async function updateWinnerTable(): Promise<void> {
   const page = +winner.children[1].firstElementChild.textContent.slice(1);
@@ -11,5 +14,16 @@ export default async function updateWinnerTable(): Promise<void> {
     el.classList.contains("winners__table"),
   ).lastElementChild;
   tbody.innerHTML = "";
-  addWinners(tbody, page);
+  const thead = [...winner.children].find((el) =>
+    el.classList.contains("winners__table"),
+  ).firstElementChild;
+  const activeSort: HTMLElement = thead.querySelector(".active");
+  if (activeSort) {
+    const { sortOrder, column } = activeSort.dataset;
+    const sortedWinners = await getSortedWinners(page, column, sortOrder);
+    const edittedSortedWinners = await editWinners(sortedWinners);
+    fillTable(edittedSortedWinners, page, tbody);
+  } else {
+    addWinners(tbody, page);
+  }
 }
