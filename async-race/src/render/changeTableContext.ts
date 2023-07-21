6d4b9b7e566250908bca1d1cd00d561828b27da6
Katsiaryna <katsiaryna.n@gmail.com>
2023-07-21 16:23:02 +0300

    refactor: separate winners table updating

---
+++ async-race/src/render/changeTableContext.ts
@@ -0,0 +1,42 @@
+import { editWinners } from "../api/getData";
+import getSortedWinners from "../api/getSortedWinners";
+import { addWinners } from "../components/winner/winners-table";
+import fillTable from "./fillTable";
+
+function checkActiveSort(place: HTMLElement): HTMLElement | null {
+  const table = [...place.children].find((el) =>
+    el.classList.contains("winners__table"),
+  );
+  const active = table.querySelector(".active");
+  const activeSort = active as HTMLElement;
+  return activeSort;
+}
+
+async function addSortedWinners(
+  activeSort: HTMLElement,
+  page: number,
+  tbody: Element,
+): Promise<void> {
+  const toSort = activeSort.dataset.column;
+  const { sortOrder } = activeSort.dataset;
+  const sortedWinners = await getSortedWinners(page, toSort, sortOrder);
+  const edittedSortedwinner = await editWinners(sortedWinners);
+  fillTable(edittedSortedwinner, page, tbody);
+}
+
+const changeTableContent = (place: HTMLElement, page: number): void => {
+  const activeSort = checkActiveSort(place);
+  console.log(place);
+  const tbody = [...place.children].find((el) =>
+    el.classList.contains("winners__table"),
+  ).lastElementChild;
+
+  tbody.innerHTML = "";
+  if (activeSort) {
+    addSortedWinners(activeSort, page, tbody);
+  } else {
+    addWinners(tbody, page);
+  }
+};
+
+export default changeTableContent;
--- async-race/src/render/updateWinnerTable.ts
+++ async-race/src/render/updateWinnerTable.ts
@@ -1,29 +1,11 @@
-import { editWinners, getWinners } from "../api/getData";
-import getSortedWinners from "../api/getSortedWinners";
+import { getWinners } from "../api/getData";
 import winner from "../components/winner/winners";
-import { addWinners } from "../components/winner/winners-table";
-import fillTable from "./fillTable";
-// import { Winner } from "../types/type";
+import changeTableContent from "./changeTableContext";
 
 export default async function updateWinnerTable(): Promise<void> {
   const page = +winner.children[1].firstElementChild.textContent.slice(1);
   const winnersList = await getWinners();
   const heading = winner.firstElementChild.firstElementChild;
   heading.textContent = `(${winnersList.length})`;
-  const tbody = [...winner.children].find((el) =>
-    el.classList.contains("winners__table"),
-  ).lastElementChild;
-  tbody.innerHTML = "";
-  const thead = [...winner.children].find((el) =>
-    el.classList.contains("winners__table"),
-  ).firstElementChild;
-  const activeSort: HTMLElement = thead.querySelector(".active");
-  if (activeSort) {
-    const { sortOrder, column } = activeSort.dataset;
-    const sortedWinners = await getSortedWinners(page, column, sortOrder);
-    const edittedSortedWinners = await editWinners(sortedWinners);
-    fillTable(edittedSortedWinners, page, tbody);
-  } else {
-    addWinners(tbody, page);
-  }
+  changeTableContent(winner, page);
 }
