9e4feb5a9fe7c9c00e072917c7295cb4d66fb531
Katsiaryna <katsiaryna.n@gmail.com>
2023-07-21 19:41:12 +0300

    refactor: creating winners table

--- async-race/src/components/winner/winners-table.ts
+++ async-race/src/components/winner/winners-table.ts
@@ -1,9 +1,38 @@
-/* eslint-disable max-lines-per-function */
 import { editWinners } from "../../api/getData";
 import { getWinnersPage } from "../../api/getPage";
-import carImage from "../../assets/icons/car.";
 import sortTable from "../../callbacks/sort";
+import fillTable from "../../render/fillTable";
 import createElement from "../../utils/createElement";
+
+const createSortedTH = (headingElements: HTMLElement[]): void => {
+  const sortTH = [headingElements[3], headingElements[4]];
+  const columnDataset = ["wins", "time"];
+  sortTH.forEach((th, i) => {
+    const arrowCont = createElement({
+      tag: "div",
+      className: ["sort"],
+    });
+
+    const sortUp = createElement({
+      tag: "div",
+      className: ["sort__up"],
+    });
+    sortUp.dataset.sortOrder = "ASC";
+    sortUp.dataset.column = columnDataset[i];
+    sortUp.addEventListener("click", sortTable);
+
+    const sortDown = createElement({
+      tag: "div",
+      className: ["sort__down"],
+    });
+    sortDown.dataset.sortOrder = "DESC";
+    sortDown.dataset.column = columnDataset[i];
+    sortDown.addEventListener("click", sortTable);
+
+    arrowCont.append(sortUp, sortDown);
+    th.append(arrowCont);
+  });
+};
 
 const createTableHead = (): HTMLElement => {
   const head = createElement({
@@ -12,7 +41,6 @@ const createTableHead = (): HTMLElement => {
   const row = createElement({
     tag: "tr",
   });
-
   const headings: string[] = ["№", "Car", "Name", "Wins", "Best time"];
   const headingElements = headings.map((el) =>
     createElement({
@@ -20,32 +48,7 @@ const createTableHead = (): HTMLElement => {
       text: el,
     }),
   );
-  const sortTH = [headingElements[3], headingElements[4]];
-  const columnDataset = ["wins", "time"];
-
-  sortTH.forEach((th, i) => {
-    const arrowCont = createElement({
-      tag: "div",
-      className: ["sort"],
-    });
-    const sortUp = createElement({
-      tag: "div",
-      className: ["sort__up"],
-    });
-    sortUp.dataset.sortOrder = "ASC";
-    sortUp.dataset.column = columnDataset[i];
-    sortUp.addEventListener("click", sortTable);
-    const sortDown = createElement({
-      tag: "div",
-      className: ["sort__down"],
-    });
-    sortDown.dataset.sortOrder = "DESC";
-    sortDown.dataset.column = columnDataset[i];
-    arrowCont.append(sortUp, sortDown);
-    sortDown.addEventListener("click", sortTable);
-    th.append(arrowCont);
-  });
-
+  createSortedTH(headingElements);
   row.append(...headingElements);
   head.append(row);
   return head;
@@ -71,33 +74,5 @@ export async function addWinners(
   const winners = await getWinnersPage(pageNumber);
   const editedWinners = await editWinners(winners);
 
-  let n = (pageNumber - 1) * 10 + 1;
-  editedWinners.forEach((winner) => {
-    const row = createElement({ tag: "tr" });
-    row.setAttribute("data-id", `${winner.id}`);
-    const numberTD = createElement({
-      tag: "td",
-      text: `${n}`,
-    });
-    n += 1;
-    const carTD = createElement({
-      tag: "td",
-    });
-    carTD.innerHTML = carImage;
-    carTD.firstElementChild.setAttribute("fill", winner.color);
-    const nameTD = createElement({
-      tag: "td",
-      text: winner.name,
-    });
-    const winsTD = createElement({
-      tag: "td",
-      text: `${winner.wins}`,
-    });
-    const timeTd = createElement({
-      tag: "td",
-      text: `${winner.time}`,
-    });
-    row.append(numberTD, carTD, nameTD, winsTD, timeTd);
-    placeToAppend.append(row);
-  });
+  fillTable(editedWinners, pageNumber, placeToAppend);
 }
