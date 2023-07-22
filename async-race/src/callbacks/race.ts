2171a4e62aace582bc7a6ebd41c2e8684eed8b71
Katsiaryna <katsiaryna.n@gmail.com>
2023-07-22 15:07:24 +0300

    fix: add error catching

--- async-race/src/callbacks/race.ts
+++ async-race/src/callbacks/race.ts
@@ -1,6 +1,6 @@
 import { getGaragePage } from "../api/getPage";
 import startEngine from "../api/startEngine";
-import addWinMessage from "../components/message/message-view";
+import addWinMessage, { message } from "../components/message/message-view";
 import { Car } from "../types/type";
 import addWinner from "../api/addWinner";
 import { startDrive } from "./animation";
@@ -22,7 +22,6 @@ export default async function startRace(e: Event): Promise<Car> {
   const raceBtn = e.target as HTMLButtonElement;
   const resetBtn = raceBtn.nextElementSibling as HTMLButtonElement;
   const generateBtn = resetBtn.nextElementSibling as HTMLButtonElement;
-
   const garage = raceBtn.closest(".select").nextElementSibling;
   const buttons = [
     raceBtn,
@@ -34,9 +33,7 @@ export default async function startRace(e: Event): Promise<Car> {
   const page = +garage.children[1].firstElementChild.textContent.slice(1);
   const currentCars = await getGaragePage(page);
   const promisedCars = currentCars.map((car) => startEngine(car.id));
-
   const carParams = await Promise.all(promisedCars);
-  console.log(carParams);
   const promisedCarParams = carParams.map((params) =>
     startDrive(params, garage),
   );
@@ -46,13 +43,18 @@ export default async function startRace(e: Event): Promise<Car> {
   setTimeout(() => {
     enableButtons(buttons);
   }, maxTime);
-  const winnerID = await Promise.any(promisedCarParams);
-  const winnerCar = currentCars.find((el) => el.id === winnerID);
-  const winnerParams = carParams.find((param) => param.id === winnerID);
-  const winner = { ...winnerCar, ...winnerParams };
-  console.log(winner);
-  addWinMessage(winner);
-  addWinner(winner);
-
+  let winner: Car;
+  try {
+    const winnerID = await Promise.any(promisedCarParams);
+    const winnerCar = currentCars.find((el) => el.id === winnerID);
+    const winnerParams = carParams.find((param) => param.id === winnerID);
+    winner = { ...winnerCar, ...winnerParams };
+    console.log(winner);
+    addWinMessage(winner);
+    addWinner(winner);
+  } catch {
+    message.textContent = "All cars have been broken";
+    enableButtons(buttons);
+  }
   return winner;
 }
